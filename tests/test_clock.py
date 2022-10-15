9e8ce06e5bea107770fa1d30675b8903ed339259
Sean Rawson <seancc8@gmail.com>
2022-10-15 17:56:08 -0400

    Add/update tests in 'test_clock.py'

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -3,9 +3,12 @@ import os
 import unittest
 import tkinter as tk
 
+from unittest.mock import MagicMock, patch
+
 sys.path.insert(0, os.path.abspath('..'))
 
 from clock import Clock
+from texttwistgame import TextTwistGame
 
 class TestClock(unittest.TestCase):
 
@@ -14,15 +17,24 @@ class TestClock(unittest.TestCase):
         self.clock = Clock()
 
     def test_clock_default_time(self):
-        self.assertEqual(self.clock.get_time(), 120)
+        self.assertEqual(self.clock._get_time(), 120)
 
-    def test_get_time_string(self):
-        self.assertEqual(self.clock.get_time_string(), "2:00")
+    def test_repr(self):
+        self.assertEqual(str(self.clock), "2:00")
 
     def test_add_observer(self):
-        self.clock.add_observer(self)
-        self.assertIsInstance(self.clock._Clock__observers.pop(),
-                TestClock)
+        mock = MagicMock(TextTwistGame)
+
+        self.clock.add_observer(mock.notify_clock_reached_zero)
+        self.clock._notify_clock_reached_zero()
+        mock.notify_clock_reached_zero.assert_called_once()
+
+    @patch.object(Clock, "_notify_clock_reached_zero")
+    def test_run_clock_to_zero(self, func):
+        self.clock = Clock(1)
+        self.clock.run()
+
+        self.assertTrue(func.called)
 
     def tearDown(self):
         self.root.destroy()
