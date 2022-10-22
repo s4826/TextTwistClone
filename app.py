e6c18df46724202e54214f5a3761459d72d0aa95
Sean Rawson <seancc8@gmail.com>
2022-10-22 16:02:51 -0400

    Catch exceptions raised from child threads

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
+import threading
 from texttwistgame import TextTwistGame
 from ui import TextTwistUI
+
+
+def handle_thread_exceptions(*args):
+    """
+    Consume any exceptions from spawned threads
+
+    The game only spawns a single clock thread. Exiting the program
+    while this clock thread is running raises a runtime exception,
+    which we don't need to handle (in the current iteration
+    of this program).
+    """
+    pass
 
 
 if __name__ == "__main__":
     """
     Main application start location
     """
+
+    # redefine hook for thread exceptions
+    threading.excepthook = handle_thread_exceptions
+
     ui = TextTwistUI()
     game = TextTwistGame()
 
